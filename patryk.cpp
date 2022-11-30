19fc6fcf4dae9227b9d914c62516237d40c03ef3
papik693 <patrykheyn@gmail.com>
2022-11-30 18:24:01 +0100

    fixy do dodawania, usuwania

--- patryk.cpp
+++ patryk.cpp
@@ -3,7 +3,14 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 using namespace std;
-float wartosc_zamowienia=0;
+int wartosc_zamowienia=0;
+int wybor_w_menu=1, ilosc_porcji, numer_do_usuniecia;
+ifstream plik("menu.json");
+json menu = json::parse(plik);
+ifstream plik2("zamowienie.json");
+json zamowienie = json::parse(plik2);
+
+
 void wyswietl_menu(json menu){
     cout << "*** MENU ***" << endl;
     int j = 0;
@@ -13,45 +20,57 @@ void wyswietl_menu(json menu){
         j++;
     }
 }
-void wyswietl_zamowienie(json zamowienie){
-    int j = 0;
+void wyswietl_zamowienie(){
+    int j = 0, wartosc_zamowienia=0;
     for (auto i : zamowienie["Zamowienie"]){
         cout << j+1 << ". " << zamowienie["Zamowienie"][j]["nazwa"].get<string>() << ", cena: " << zamowienie["Zamowienie"][j]["cena"] << endl;
+        wartosc_zamowienia = wartosc_zamowienia + zamowienie["Zamowienie"][j]["cena"].get<int>();
         j++;
-        wartosc_zamowienia+= zamowienie["Zamowienie"][j]["cena"];
     }
-    cout << wartosc_zamowienia;
+    cout << "Całkowita wartość zamówienia wynosi: " << wartosc_zamowienia << endl;
+}
+
+void usun_z_zamowienia(int numer_do_usuniecia){
+    numer_do_usuniecia = numer_do_usuniecia-1; // aby wyrównać do indeksu w tabeli
+    zamowienie["Zamowienie"].erase(zamowienie["Zamowienie"].begin() + numer_do_usuniecia);
+}
+
+void dodaj_do_zamowienia(int wybor_w_menu){
+    wybor_w_menu = wybor_w_menu-1; // aby wyrównać do indeksu w tabeli
+    cout << "Ile porcji?: ";
+    cin >> ilosc_porcji;
+    for (int i=0;i<ilosc_porcji;i++){
+        zamowienie["Zamowienie"].push_back(menu["Dania"][wybor_w_menu]);
+    }
 }
 
 int main() {
-    int wybor=1, ilosc;
-    float wartosc_zamowienia=0;
-    ifstream plik("menu.json");
-    json menu = json::parse(plik);
-    ifstream plik2("zamowienie.json");
-    json zamowienie = json::parse(plik2);
-    while (wybor != 0){
+    while (wybor_w_menu != 0){
         wyswietl_menu(menu);
-        cout << "Które danie wybierasz(numerek)? Chyba że wolisz przejść do podsumowania - wtedy naciśnij 0. Aby wyświetlić zamówienie lub usunąć pozycję z obecnego zamówienie wciśnij 100." << endl << "Twój wybór: "; 
-        cin >> wybor;
-        switch (wybor)
+        cout << "Które danie wybierasz(numerek)? Chyba że wolisz przejść do podsumowania - wtedy naciśnij 0. Aby wyświetlić zamówienie wciśnij 100 lub aby usunąć pozycję z obecnego zamówienie wciśnij 200" << endl << "Twój wybór: "; 
+        cin >> wybor_w_menu;
+        switch (wybor_w_menu)
         {
         case 0:
             cout << "Wyjście z zamówień" << endl;
             break;    
         case 100:
             cout << "Twoje obecne zamowienie to:" << endl;
-            wyswietl_zamowienie(zamowienie);
+            wyswietl_zamowienie();
             cout << "Czy chcesz przejść do podsumowania? Jeśli tak wciśnij 0, aby wyświetlić menu wybierz dowolną liczbę" << endl << "Twój wybór: ";
-            cin >> wybor;
+            cin >> wybor_w_menu;
+            break;
+        case 200:
+            cout << "Twoje obecne zamowienie to:" << endl;
+            wyswietl_zamowienie();
+            cout << "Którą pozycję chcesz usunąć?" << endl;
+            cin >> numer_do_usuniecia;
+            usun_z_zamowienia(numer_do_usuniecia);
             break;
         default:
-            bool istnieje = menu["Dania"][wybor].contains("nazwa");
-            cout << istnieje << endl;
+            bool istnieje = menu["Dania"][wybor_w_menu-1].contains("nazwa");
             if (istnieje != 0) {
-                cout << "Ile porcji?: ";
-                cin >> ilosc;
-                zamowienie["Zamowienie"].push_back(menu["Dania"][wybor]);
+                dodaj_do_zamowienia(wybor_w_menu);
             } else { 
                 cout << "Nie ma takiej pozycji w karcie. Wybierz inną" << endl << endl;
             }
